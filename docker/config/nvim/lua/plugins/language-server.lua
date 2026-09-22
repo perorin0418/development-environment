5fb3feede54c000ca2ff3e451396c62cebfb1a54
developer <developer@local>
2026-09-22 08:05:26 +0000

    jcode temp dir fix

--- docker/config/nvim/lua/plugins/language-server.lua
+++ docker/config/nvim/lua/plugins/language-server.lua
@@ -22,18 +22,14 @@ return {
       ensure_installed = { "lua_ls" },
     },
     config = function(_, opts)
-      require("mason-lspconfig").setup(opts)
-
-      -- mason でインストールした各 LS の setup を自動で呼び出す。
-      -- これを書いていない場合、自分で
-      -- require("lspconfig").<サーバ名>.setup({}) をそれぞれ書く必要がある。
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
-        end,
+      -- mason-lspconfig v2 以降は setup_handlers が廃止され、
+      -- インストール済み LS は自動で有効化される。共通設定は
+      -- vim.lsp.config("*", ...) で与える。
+      vim.lsp.config("*", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
       })
+
+      require("mason-lspconfig").setup(opts)
 
       -- LS から受け取ったエラーなどの診断情報を表示する。
       vim.diagnostic.config({ virtual_text = true })
